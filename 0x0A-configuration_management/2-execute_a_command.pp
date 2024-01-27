25018aab7393107c18f3dfcf1e97cd331e042197
Naouwa <densali.nahla@gmail.com>
2024-01-27 21:51:56 +0100

    removing projects

--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,7 +0,0 @@
-# Create a manifest that kills a process named killmenow.
-
-exec { 'pkill':
-  command => 'pkill killmenow',
-  onlyif  => 'pgrep killmenow',
-  path    => ['/usr/bin', '/usr/sbin', '/bin'],
-}
