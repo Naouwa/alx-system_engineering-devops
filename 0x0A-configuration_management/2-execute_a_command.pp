45e1de7284578905e1110e438f3f74facdb32756
Naouwa <densali.nahla@gmail.com>
2024-05-03 14:45:38 +0100

    last task

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,7 @@
+# Creates a manifest that kills a process named killmenow.
+
+exec { 'pkill':
+  command => 'pkill killmenow',
+  onlyif  => 'pgrep killmenow',
+  path    => ['/usr/bin', '/usr/sbin', '/bin']
+}
